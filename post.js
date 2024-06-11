aa9722dcf3fc0a2bc439298beff910d57b6a6255
Jesus Mimila Hurtado <166437832+JesusMimilaHurtado@users.noreply.github.com>
2024-06-11 09:26:31 -0700

    created form and added js base
    created form and added js base from edit exercise

---
+++ post.js
@@ -0,0 +1,50 @@
+"use strict"
+
+const postButton = document.getElementById('postButton');
+
+const updatedInfo = document.getElementById('updateInfo');
+
+const department = document.getElementById('dept');
+const courseNum = document.getElementById('courseNum');
+const courseName = document.getElementById('courseName');
+const instructor = document.getElementById('instr');
+const startDate = document.getElementById('startDate');
+const numDays = document.getElementById('numDays');
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    postButton.addEventListener('click', getData)
+
+})
+
+function getData(){
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("dept", department.value);
+    urlencoded.append("courseNum", courseNum.value);
+    urlencoded.append("courseName", courseName.value);
+    urlencoded.append("instructor", instructor.value);
+    urlencoded.append("startDate", startDate.value);
+    urlencoded.append("numDays", numDays.value);
+
+    const requestOptions = {
+        method: "POST",
+        body: urlencoded,
+        redirect: "follow"
+    };
+
+    fetch('http://localhost:8090/api/courses', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        const info = `
+            Department: ${data.dept}
+            Course Number: ${data.courseNum}
+            Course Name: ${data.courseName}
+            Instructor: ${data.instructor}
+            Start Date: ${data.startDate}
+            Number of Days: ${data.numDays}`
+
+        updatedInfo.innerHTML = info
+    })
+    .catch((error) => console.error(error));
+}
